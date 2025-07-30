22fbb7db60a084fd6e29ed94495eae12649d143a
Jonathan Windgassen <j.windgassen@fz-juelich.de>
2025-07-31 00:38:10 +0200

    Added LintRule for LightingNeedsToBeRebuilt

---
+++ Source/Linter/Private/LintRules/LintRule_Level_LightingNeedsToRebuilt.cpp
@@ -0,0 +1,32 @@
+﻿// Copyright (c) 2021-2025, Forschungszentrum Jülich GmbH. All rights reserved.
+
+
+#include "LintRules/LintRule_Level_LightingNeedsToRebuilt.h"
+#include "EngineModule.h"
+
+ULintRule_Level_LightingNeedsToRebuilt::ULintRule_Level_LightingNeedsToRebuilt(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer) {
+    bRequiresGameThread = true;
+}
+
+bool ULintRule_Level_LightingNeedsToRebuilt::PassesRule(UObject* ObjectToLint, const ULintRuleSet* ParentRuleSet, TArray<FLintRuleViolation>& OutRuleViolations) const {
+    if (!Cast<UWorld>(ObjectToLint)) {
+        return true;
+    }
+    
+    return Super::PassesRule(ObjectToLint, ParentRuleSet, OutRuleViolations);
+}
+
+bool ULintRule_Level_LightingNeedsToRebuilt::PassesRule_Internal_Implementation(UObject* ObjectToLint, const ULintRuleSet* ParentRuleSet, TArray<FLintRuleViolation>& OutRuleViolations) const {
+    UWorld* World = CastChecked<UWorld>(ObjectToLint);
+    
+    GetRendererModule().UpdateMapNeedsLightingFullyRebuiltState(World);
+    if (World->NumLightingUnbuiltObjects > 0 || World->NumUnbuiltReflectionCaptures > 0) {
+        const FText RecommendedAction = NSLOCTEXT("Linter", "LintRule_Level_LightingNeedsToRebuilt", "Rebuild the Lighting of {0}");
+        OutRuleViolations.Push(
+            FLintRuleViolation(ObjectToLint, GetClass(), FText::FormatOrdered(RecommendedAction, FText::FromString(World->GetMapName())))
+        );
+        return false;
+    }
+    
+    return true;
+}
---
+++ Source/Linter/Public/LintRules/LintRule_Level_LightingNeedsToRebuilt.h
@@ -0,0 +1,21 @@
+﻿// Copyright (c) 2021-2025, Forschungszentrum Jülich GmbH. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "LintRule.h"
+#include "LintRule_Level_LightingNeedsToRebuilt.generated.h"
+
+
+UCLASS(BlueprintType, Blueprintable)
+class LINTER_API ULintRule_Level_LightingNeedsToRebuilt : public ULintRule {
+    GENERATED_BODY()
+
+public:
+    ULintRule_Level_LightingNeedsToRebuilt(const FObjectInitializer& ObjectInitializer);
+    
+    virtual bool PassesRule(UObject* ObjectToLint, const ULintRuleSet* ParentRuleSet, TArray<FLintRuleViolation>& OutRuleViolations) const override;
+    
+protected:
+    virtual bool PassesRule_Internal_Implementation(UObject* ObjectToLint, const ULintRuleSet* ParentRuleSet, TArray<FLintRuleViolation>& OutRuleViolations) const override;
+};
